c38674ba3078cbddd90f3403d9cf6b6e31212b40
akitig <akitig24@gmail.com>
2024-04-27 09:03:25 +0900

    fix main

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: akunimot <akunimot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/27 05:46:09 by akunimot          #+#    #+#             */
-/*   Updated: 2024/04/27 07:23:39 by akunimot         ###   ########.fr       */
+/*   Updated: 2024/04/27 09:02:10 by akunimot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 void	ft_printf_format(va_list args, char format)
 {
 	if (format == 'd')
-		printf("%d\n", va_arg(args, int));
+		ft_putnbr_fd(va_arg(args, int), 1);
 	else
 		(void)args;
 }
@@ -43,22 +43,76 @@ int	ft_printf(const char *str, ...)
 
 int	main(void)
 {
-	printf("printf    :%c\n", 'c');
-	ft_printf("ft_printf :%c\n", 'c');
-	printf("printf    :%s\n", "abcde");
-	ft_printf("ft_printf :%s\n", "abcde");
-	printf("printf    : %p\n", "abcde");
-	ft_printf("ft_printf :%p\n", "abcde");
-	printf("printf    : %d\n", INT_MAX);
-	ft_printf("ft_printf : %d\n", INT_MAX);
-	printf("printf    : %i\n", INT_MAX);
-	ft_printf("ft_printf : %i\n", INT_MAX);
-	printf("printf    : %u\n", UINT_MAX);
-	ft_printf("ft_printf : %u\n", UINT_MAX);
-	printf("printf    : %x\n", 3);
-	ft_printf("ft_printf : %x\n", 3);
-	printf("printf    : %X\n", 3);
-	ft_printf("ft_printf : %X\n", 3);
-	printf("printf    : %%");
-	ft_printf("ft_printf : %%");
+	printf("format c : 'c'\n");
+	printf("%c", 'c');
+	printf("\n");
+	ft_printf("%c", 'c');
+	printf("\n");
+
+	printf("format s : \"abcde\"\n");
+	printf("%s", "abcde");
+	printf("\n");
+	ft_printf("%s", "abcde");
+	printf("\n");
+
+	printf("format p : \"abcde\"\n");
+	printf("%p", "abcde");
+	printf("\n");
+	ft_printf("%p", "abcde");
+	printf("\n");
+
+	printf("format d : 12345\n");
+	printf("%d", 12345);
+	printf("\n");
+	ft_printf("%d", 12345);
+	printf("\n");
+
+	printf("format i : 12345\n");
+	printf("%i", 12345);
+	printf("\n");
+	ft_printf("%i", 12345);
+	printf("\n");
+
+	printf("format u : UINT_MAX\n");
+	printf("%u", UINT_MAX);
+	printf("\n");
+	ft_printf("%u", UINT_MAX);
+	printf("\n");
+
+	printf("format x : 3\n");
+	printf("%x", 3);
+	printf("\n");
+	ft_printf("%x", 3);
+	printf("\n");
+
+	printf("format X : 3\n");
+	printf("%X", 3);
+	printf("\n");
+	ft_printf("%X", 3);
+	printf("\n");
+
+	printf("fotmat percent : percent\n");
+	printf("%%");
+	printf("\n");
+	ft_printf("%%");
+	printf("\n");
+
+	// printf("printf    :%c\n", 'c');
+	// ft_printf("ft_printf :%c\n", 'c');
+	// printf("printf    :%s\n", "abcde");
+	// ft_printf("ft_printf :%s\n", "abcde");
+	// printf("printf    : %p\n", "abcde");
+	// ft_printf("ft_printf :%p\n", "abcde");
+	// printf("printf    : %d\n", INT_MAX);
+	// ft_printf("ft_printf : %d\n", INT_MAX);
+	// printf("printf    : %i\n", INT_MAX);
+	// ft_printf("ft_printf : %i\n", INT_MAX);
+	// printf("printf    : %u\n", UINT_MAX);
+	// ft_printf("ft_printf : %u\n", UINT_MAX);
+	// printf("printf    : %x\n", 3);
+	// ft_printf("ft_printf : %x\n", 3);
+	// printf("printf    : %X\n", 3);
+	// ft_printf("ft_printf : %X\n", 3);
+	// printf("printf    : %%");
+	// ft_printf("ft_printf : %%");
 }
